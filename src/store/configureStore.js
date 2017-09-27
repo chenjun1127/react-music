1a93f6262067db0ba4115efd9493e0af4dd3420a
jone <kiss19861127@126.com>
2017-09-27 21:21:02 +0800

    update

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers/index';
 import thunkMiddleware from 'redux-thunk';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const configureStore = (initialState) => {
     return createStore(
         rootReducer,
         initialState,
-        compose(applyMiddleware(thunkMiddleware), window.devToolsExtension ? window.devToolsExtension() : undefined)
+        // compose(applyMiddleware(thunkMiddleware), window.devToolsExtension && window.devToolsExtension())
+        composeEnhancers(applyMiddleware(thunkMiddleware))
     );
 };
 export default configureStore;
